ac77d9c0a060447cdaddfa5eaf2022a2c855d244
ersin <ersin-kaya@outlook.com.tr>
2024-10-30 22:56:46 +0300

    try...catch is really not ideal

--- best-practices/ditching-try-catch/script.ts
+++ best-practices/ditching-try-catch/script.ts
@@ -13,9 +13,14 @@ async function getUser(id: number) {
   return { id, name: "Kyle" };
 }
 
-const user = await getUser(1);
-
-// Lots of code that users the user
-console.log(user);
+try {
+  // 1. User variable is scoped to the try block and cannot be accessed outside of it!
+  const user = await getUser(1);
+  // Lots of code that users the user
+  console.log(usr); // 2. Attempting to log 'usr' (which is undefined) will cause a ReferenceError and trigger the catch block,
+  // even though id !== 2
+} catch (error) {
+  console.log("There was an error");
+}
 
 export {};
